3f2d4786b7e0b99d85647e2544489690a838b24e
joknarf <joknarf@free.fr>
2025-05-19 07:54:00 +0200

    command completion

--- complete-ng.plugin.bash
+++ complete-ng.plugin.bash
@@ -64,7 +64,7 @@ _complete-ng_key() {
 
 _complete-ng() {
   local cmd="${COMP_WORDS[O]}" fn IFS="$IFS" opt="-f" word="" selopt='-o filenames' longword
-  [ "$COMP_CWORD" -ge 0 ] && word="${COMP_WORDS[$COMP_CWORD]}"
+  [ ${#COMP_WORDS[@]} -gt 0 ] && word="${COMP_WORDS[$COMP_CWORD]}"
   fn=$(eval printf %s '$'_compfunc_"${cmd//[^a-zA-Z0-9_]/_}")
   [ "$fn" ] || { cmd="${cmd##*/}"; fn=$(eval printf %s '$'_compfunc_"${cmd//[^a-zA-Z0-9_]/_}"); }
   [ "$fn" ] || {
